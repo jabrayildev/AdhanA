6bd11de6e0910fbdfb65641255d8a4c24000ed41
Jabrayil Gulamzada <c.gulamzadad@gmail.com>
2026-05-05 00:13:35 +0400

    Initial Commit

---
+++ AdhanA/AdhanAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdhanAApp.swift
+//  AdhanA
+//
+//  Created by Джабраил on 05.05.26.
+//
+
+import SwiftUI
+
+@main
+struct AdhanAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
